43088ae71a3e3ef8f09e27ec842112ac6eea6356
baileytorch <baileytorch@hotmail.com>
2026-04-01 13:50:45 -0300

    Primera carga de archivos

---
+++ tipos_datos.py
@@ -0,0 +1,17 @@
+# Tipos de datos en Python
+
+# Con el símbolo # escribo comentarios que no se ejecutan con el código
+
+# Tipo de dato STRING str
+print('Hola')
+# Tipo de dato INTEGER int
+print(25)
+# Tipo de dato FLOATING float
+print(2.5)
+# Tipo de dato BOOLEAN bool
+print(False)
+
+print(type('Hola'))
+print(type(25))
+print(type(2.5))
+print(type(False))
