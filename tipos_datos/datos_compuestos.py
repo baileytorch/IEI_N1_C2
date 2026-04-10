92aeff91a0ca443757dc9047d40ed816475f2a0f
baileytorch <baileytorch@hotmail.com>
2026-04-10 11:10:31 -0400

    Trabajando con colecciones de datos

---
+++ tipos_datos/datos_compuestos.py
@@ -0,0 +1,14 @@
+# Colecciones de DATOS
+
+# LISTAS
+# Es una colección ORDENADA y MUTABLE de datos de cualquiere tipo
+
+print('Listas en Python')
+mi_primera_lista = ['Erick Bailey',50,True]
+
+nombre_personal = input('Ingrese su nombre: ')
+print(type(mi_primera_lista))
+print(mi_primera_lista)
+print(f'El primer elemento de la lista es: {mi_primera_lista[0]}')
+mi_primera_lista[0] = nombre_personal
+print(mi_primera_lista)
