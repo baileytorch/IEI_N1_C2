123c907731b66cc36d1f1bb3a3e63aeeacef128e
baileytorch <baileytorch@hotmail.com>
2026-04-29 13:19:46 -0400

    Actualizando funciones aritméticas

--- funciones/calculadora.py
+++ funciones/calculadora.py
@@ -11,21 +11,30 @@
 # los insumos que necesitará la función para trabajar
 print('\nOperaciones aritméticas con 2 números mediante métodos')
 def suma(a,b):
-    resultado = a + b
-    print(f'{a} + {b} = {resultado}')
+    try:
+        resultado = a + b
+        return resultado
+    except Exception as error:
+        print(f'No se puede realizar la operación, por el siguiente error: {error}')
 
 def resta(a,b):
-    resultado = a - b
-    print(f'{a} - {b} = {resultado}')
+    try:
+        resultado = a - b
+        return resultado
+    except Exception as error:
+        print(f'No se puede realizar la operación, por el siguiente error: {error}')
 
 def multiplicacion(a,b):
-    resultado = a * b
-    print(f'{a} x {b} = {resultado}')
+    try:
+        resultado = a * b
+        return resultado
+    except Exception as error:
+        print(f'No se puede realizar la operación, por el siguiente error: {error}')
 
 def division(a,b):
     try:
         resultado = a / b
-        print(f'{a} / {b} = {resultado}')
+        return resultado
     except ZeroDivisionError:
         print('No se puede dividir por 0.')
     except Exception as error:
@@ -33,8 +42,8 @@ def division(a,b):
 
 def convertir_float(valor):
     try:
-        resultado = float(valor)
-        return resultado
+        num_decimal = float(valor)
+        return num_decimal
     except ValueError: 
             print('No se puede convertir el valor ingresado a decimal')
     except Exception as error:
