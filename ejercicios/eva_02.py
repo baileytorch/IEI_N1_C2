5cc2b9a17f3bae5d4cbc5e9453639b045d3ed59f
baileytorch <baileytorch@hotmail.com>
2026-05-06 13:49:23 -0400

    ejercicios eva 2

---
+++ ejercicios/eva_02.py
@@ -0,0 +1,22 @@
+# Resuelva los siguientes ejercicios mediante funciones,
+# las que serán llamadas dentro de un menú de opciones.
+
+# 1.- Cree una función que entregue el saludo "Buen día!"
+
+# 2.- Cree una función que solicite el nombre y el género al usuario y luego, 
+# llamando a la función anterior, salude al usuario, el saludo debe quedar así: 
+# "Buen día nombre_usuario!", 
+# y agregue el texto "Hoy te ves hermosa" si el usuario es mujer 
+# o "cómo estás campeón?" si el usuario es hombre
+
+# 3.- Cree una función que permita calcular el área de una circunferencia 
+# y otra que permita calcular el volumen de un cilindro usando la función anterior
+# los datos deben ser ingresados por el usuario
+
+# 4.- Cree una función que pida un número entero al usuario 
+# y calcule el factorial de ese número
+
+def menu_principal():
+    pass
+
+menu_principal()
